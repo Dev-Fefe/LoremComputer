735efe912def41e06d3a66f48f903bbc1af31873
federico morchio <federico.morchio@gmail.com>
2024-12-05 12:24:14 +0100

    WorkInProgress

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
-
-Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
-Route::get('/article/index', [ArticleController::class, 'index'])->name('article.index');
-Route::get('/article/edit/{article}', [ArticleController::class, 'edit'])->name('article.edit');
